4f18ba655bc8aa3ba37d0c53c58eb29048c593e0
codestar <glegat@gaming.tech>
2024-10-22 20:07:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,30 +7,51 @@ int main()
 {
 	int monsterHP = 100; int attackValue = 0;
 	int playerHP = 50; int blockValue = 0;
-	char attack = 'A';
-	char block = 'B';
-	char action = ' ';
-	srand(time(NULL));
+	int attack = 0;
+	char action;
+	char option;
 
 	std::cout << "------ READY TO FIGHT ? ------" << std::endl;
+	std::cout << "------ YES(Y) OR NO(N)  ------" << std::endl;
+	std::cin >> option;
+	option = toupper(option);
 	std::cout << std::endl;
 
-	std::cout << "ATTACK ?" << " " << "BLOCK ?" << std::endl;
-	std::cin >> action;
-	action = toupper(action);
-	std::cout << std::endl;
 
-	if (action == 'A')
+
+	srand(time(NULL));
+
+	while (option == 'Y')
 	{
-		int newMonsterHP;
-		attackValue = rand() % 30;
-		std::cout << "SLASH !!" << std::endl;
-		newMonsterHP = monsterHP - attackValue;
-		std::cout << "Monster health : " << newMonsterHP << std::endl;
+		std::cout << "ATTACK ?" << " " << "BLOCK ?" << std::endl;
+		std::cin >> action;
+		action = toupper(action);
+		std::cout << std::endl;
+
+		if (action == 'A')
+		{
+			attackValue = rand() % 20;
+
+			if (attackValue > 0)
+			{
+				int newMonsterHP;
+				std::cout << "SLASH !!" << std::endl;
+				newMonsterHP = monsterHP - attackValue;
+				int* tmpHP = new int[newMonsterHP];
+				std::cout << "Monster health : " << tmpHP << std::endl;
+				std::cout << std::endl;
+				continue;
+			}
+			else
+				std::cout << "MISSED !";
+				std::cout << std::endl;
+		}
+		if (action == 'B')
+		{
+			std::cout << "BLOCKED !" << std::endl;
+		}
 	}
-	else if (action == 'B')
-	{
-		std::cout << "ATTACK BLOCKED !";
-	}
+
+	
 
 }
